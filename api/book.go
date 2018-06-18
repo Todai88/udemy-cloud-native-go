0f456ce4d1fbcbcd9da29a713f59d2ff238085dd
Todai88 <joabaj88@gmail.com>
2018-06-18 21:35:57 +0100

    Section 1, Lecture 9

--- api/book.go
+++ api/book.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -37,6 +38,15 @@ func FromJSON(data []byte) Book {
 	return book
 }
 
+func writeJSON(w http.ResponseWriter, i interface{}) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+}
+
 // AllBooks returns a slice of all books
 func AllBooks() []Book {
 	values := make([]Book, len(books))
@@ -50,11 +60,90 @@ func AllBooks() []Book {
 
 // BooksHandleFunc to be used as http.HandleFunc for Book API
 func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(books)
-	if err != nil {
-		panic(err)
+	switch method := r.Method; method {
+	case http.MethodGet:
+		books := AllBooks()
+		writeJSON(w, books)
+	case http.MethodPost:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		book := FromJSON(body)
+		isbn, created := CreateBook(book)
+		if created {
+			w.Header().Add("Location", "/api/books/"+isbn)
+			w.WriteHeader(http.StatusCreated)
+		} else {
+			w.WriteHeader(http.StatusConflict)
+		}
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
 	}
+}
 
-	w.Header().Add("Content-Type", "application/json; charset=utf-8")
-	w.Write(b)
+// BookHandleFunc to be used as http.HandleFunc for Book API.
+// It marshalls an ISBN and filters the books based on it.
+func BookHandleFunc(w http.ResponseWriter, r *http.Request) {
+	// implement logic for /api/book/<isbn>
+	isbn := r.URL.Path[len("/api/books/"):]
+
+	switch method := r.Method; method {
+	case http.MethodGet:
+		book, found := GetBook(isbn)
+		if found {
+			writeJSON(w, book)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	case http.MethodPut:
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		book := FromJSON(body)
+		exists := UpdateBook(isbn, book)
+		if exists {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	case http.MethodDelete:
+		DeleteBook(isbn)
+		w.WriteHeader(http.StatusOK)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+	}
+}
+
+// GetBook returns a book for a given ISBN
+func GetBook(isbn string) (Book, bool) {
+	book, found := books[isbn]
+	return book, found
+}
+
+// CreateBook creates a book.
+func CreateBook(book Book) (string, bool) {
+	_, exists := books[book.ISBN]
+	if exists {
+		return "", false
+	}
+	books[book.ISBN] = book
+	return book.ISBN, true
+}
+
+// UpdateBook updates a book for a given ISBN
+func UpdateBook(isbn string, book Book) bool {
+	_, exists := books[isbn]
+	if exists {
+		books[isbn] = book
+	}
+	return exists
+}
+
+// DeleteBook deletes a book by a given ISBN
+func DeleteBook(isbn string) {
+	delete(books, isbn)
 }
