302206973d6fae33b86fcdc398c2d81fd7eab118
Joakim Kakaei <joabaj88@gmail.com>
2018-07-26 14:28:15 +0100

    Started advanced module

---
+++ advanced/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	engine.Run(port())
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "4001"
+	}
+	return ":" + port
+}
