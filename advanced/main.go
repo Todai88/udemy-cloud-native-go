c70cd4965ea550bfad5f58e69237f50b2034c003
Todai88 <joabaj88@gmail.com>
2018-07-30 22:21:40 +0100

    Further work on the course

--- advanced/main.go
+++ advanced/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"net/http"
-	"os"
-
+	"os" 
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,8 +10,60 @@ func main() {
 	engine := gin.Default()
 
 	engine.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+		c.String(http.StatusOK, "pong") //
 	})
+
+	engine.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello Gin Framework."})
+	})
+
+	engine.GET("/api/books", func(c *gin.Context){
+		c.JSON(http.StatusOK, AllBooks())
+	})
+
+	engine.POST("/api/books", func(c *gin.Context) {
+		var book Book 
+		if c.BindJSON(&book) == nil {
+			isbn, created := CreateBook(book)
+			if created {
+				c.Header("Location", "/api/books/" + isbn)
+				c.Status(http.StatusCreated)
+			} else {
+				c.Status(http.StatusConflict)
+			}
+		}
+	})
+
+	engine.GET("/api/books/:isbn", func(c *gin.Context) {
+		isbn := c.Params.ByName("isbn")
+		book, found := GetBook(isbn)
+		if found {
+			c.JSON(http.StatusOK, book)
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+	})
+
+	engine.PUT("/api/books/:isbn", func(c *gin.Context) {
+		isbn := c.Params.ByName("isbn")
+
+		var book Book
+		if c.BindJSON(&book) == nil {
+			exists := UpdateBook(isbn, book)
+			if exists {
+				c.Status(http.StatusOK)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
+		} 
+	})
+
+	engine.DELETE("/api/books/:isbn", func(c *gin.Context){
+		isbn := c.Params.ByName("isbn")
+		DeleteBook(isbn)
+		c.Status(http.StatusOK)
+	})
+
 
 	engine.Run(port())
 }
